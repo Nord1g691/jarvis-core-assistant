6f36692d744984c4b5d07413b81dbcf692d6ec02
Nord1g691 <94491815+Nord1g691@users.noreply.github.com>
2026-08-21 02:27:40 +0200

    V9: add safe Home Assistant action descriptors

---
+++ v9/category-actions.js
@@ -0,0 +1,24 @@
+/** V9 safe action descriptor. UI/voice layer can use this to request HA service calls. */
+(() => {
+  const domainActions = Object.freeze({
+    light: ['turn_on', 'turn_off', 'toggle'],
+    climate: ['set_temperature', 'set_hvac_mode', 'turn_on', 'turn_off'],
+    lock: ['lock', 'unlock'],
+    cover: ['open_cover', 'close_cover', 'stop_cover', 'set_cover_position'],
+    media_player: ['play_media', 'media_play_pause', 'media_stop', 'volume_set'],
+    switch: ['turn_on', 'turn_off', 'toggle']
+  });
+
+  const getActions = (entityId) => {
+    const domain = String(entityId || '').split('.')[0];
+    return domainActions[domain] || [];
+  };
+
+  const descriptor = (entityId, action, data = {}) => ({
+    entity_id: entityId,
+    action,
+    data: { ...data }
+  });
+
+  window.JARVIS_V9_ACTIONS = Object.freeze({ domainActions, getActions, descriptor });
+})();
