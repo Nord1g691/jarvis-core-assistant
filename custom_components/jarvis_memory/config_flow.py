84eac9b1d8a5b7ec7b5b91e5eac64fe8d5f13f03
Nord1g691 <94491815+Nord1g691@users.noreply.github.com>
2026-08-31 00:26:25 +0200

    Simplify JARVIS Memory config flow

--- custom_components/jarvis_memory/config_flow.py
+++ custom_components/jarvis_memory/config_flow.py
@@ -1,18 +1,12 @@
 from __future__ import annotations
 
 from homeassistant import config_entries
-from homeassistant.core import callback
 
 from . import DOMAIN
 
 
 class JarvisMemoryConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        return config_entries.OptionsFlow()
 
     async def async_step_user(self, user_input=None):
         if self._async_current_entries():
